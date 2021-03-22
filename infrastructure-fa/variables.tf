3d302fff0e1c497c8547fd80abe997de8dd9e67b
Krutarth Vanesa <vkrutarth@gmail.com>
2021-03-22 00:12:59 -0400

    updating Front end Infrastructure

---
+++ infrastructure-fa/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_access_key" {
+  default = "AKIATWUBHDWKBWN7WYDM"
+}
+variable "aws_secret_key" {
+  default = "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"
+}
+variable "aws_region" {
+  default = "us-east-1"
+}

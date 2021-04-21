de765a51dc317fa46fb0bd758ec3de458c9a1f35
Krutarth Vanesa <vkrutarth@gmail.com>
2021-04-21 19:18:54 -0400

    JWT enabled for admin operations

--- back-end/main.py
+++ back-end/main.py
@@ -9,6 +9,8 @@ import bson
 import time
 from bson.json_util import dumps
 from bson.json_util import loads
+import jwt
+from flask_api import status
 
 app = Flask(__name__)
 CORS(app)
@@ -16,7 +18,7 @@ app.secret_key = "secrets"
 db = MongoClient(
     "mongodb+srv://vkrutarth:Kmvanesa@trips.ugkzb.mongodb.net/trips?retryWrites=true&w=majority",ssl=True,ssl_cert_reqs='CERT_NONE'
 )
-
+secret_key = "devops2021"
 
 class DateTimeEncoder(json.JSONEncoder):
     def default(self, z):
@@ -28,7 +30,29 @@ class DateTimeEncoder(json.JSONEncoder):
 
 trips = dict()
 buses = dict()
-# !Admin Login Logout
+
+
+# !Encode Token
+def encode_token():
+    payload = {
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30),
+        "iat": datetime.datetime.utcnow(),
+        "usr": "admin"
+    }
+    return jwt.encode(
+        payload, secret_key , algorithm="HS256"
+    )
+
+# !Decode Token
+def decode_token(token):
+    try:
+        payload = jwt.decode(token, secret_key, algorithms=["HS256"])
+    except:
+        return "Invalid Token"
+    else:
+        return payload["usr"]
+
+# !Admin Login
 @app.route("/admin/login", methods=["POST"])
 def admin_login():
     username = request.json.get("username")
@@ -37,11 +61,14 @@ def admin_login():
     if username == "admin" and password == "admin123":
         session["username"] = username
         print(session)
-        return jsonify("Login Success")
+        response = {
+            "access_token": encode_token(),
+        }
+        return jsonify(response)
     else:
         return jsonify("Login Failed")
 
-
+# !Admin Logout
 @app.route("/admin/logout", methods=["GET"])
 def admin_logout():
     if "username" in session:
@@ -52,49 +79,82 @@ def admin_logout():
 # !Add Bus
 @app.route("/bus/new", methods=["POST"])
 def add_bus():
-        bus_id = request.json.get("bus_id")
-        start = request.json.get("start")
-        end = request.json.get("end")
-        date = request.json.get("date")
-        duration = request.json.get("duration")
-        time = request.json.get("time")
-        seats = request.json.get("seats")
-        trip = dict(
-            start=start,
-            end=end,
-            duration=duration,
-            seats=seats,
-            date=datetime.datetime.strptime(date, "%d/%m/%Y"),
-            time=time,
-            _id=str(bus_id),
-        )
-        buses[trip["_id"]] = trip
-        db["trips"]["buses"].insert_one(trip)
-        return jsonify(trip)
+    access_token = request.headers.get("auth")
+    if access_token:
+        permission = decode_token(access_token)
+        if permission != "admin":
+            return jsonify("Auth Failed: InValid or Expired JWT token"), status.HTTP_401_UNAUTHORIZED
+        else:
+            bus_id = request.json.get("bus_id")
+            start = request.json.get("start")
+            end = request.json.get("end")
+            date = request.json.get("date")
+            duration = request.json.get("duration")
+            time = request.json.get("time")
+            seats = request.json.get("seats")
+            trip = dict(
+                start=start,
+                end=end,
+                duration=duration,
+                seats=seats,
+                date=datetime.datetime.strptime(date, "%d/%m/%Y"),
+                time=time,
+                _id=str(bus_id),
+            )
+            buses[trip["_id"]] = trip
+            db["trips"]["buses"].insert_one(trip)
+            return jsonify(trip)
+    else:
+        return jsonify("Auth Failed: No JWT Token Found"), status.HTTP_401_UNAUTHORIZED
+        
 
 
 # !Get all Bus
 @app.route("/bus/all", methods=["GET"])
 def all_buses():
-    buses = list(db["trips"]["buses"].find())
-    return json.dumps(buses, cls=DateTimeEncoder)
+    access_token = request.headers.get("auth")
+    if access_token:
+        permission = decode_token(access_token)
+        if permission != "admin" :
+            return jsonify("Auth Failed: InValid or Expired JWT token"), status.HTTP_401_UNAUTHORIZED
+        else:
+            buses = list(db["trips"]["buses"].find())
+            return json.dumps(buses, cls=DateTimeEncoder)
+    else:
+        return jsonify("Auth Failed: No JWT Token Found"), status.HTTP_401_UNAUTHORIZED
 
 
 # !Delete Bus
 @app.route("/bus/delete/<bus_id>", methods=["DELETE"])
 def delete_bus(bus_id):
-    db["trips"]["buses"].delete_one({"_id": str(bus_id)})
-    return jsonify("success")
+    access_token = request.headers.get("auth")
+    if access_token:
+        permission = decode_token(access_token)
+        if permission != "admin":
+            return jsonify("Auth Failed: InValid or Expired JWT token"), status.HTTP_401_UNAUTHORIZED
+        else:
+            db["trips"]["buses"].delete_one({"_id": str(bus_id)})
+            return jsonify("success")
+    else:
+        return jsonify("Auth Failed: No JWT Token Found"), status.HTTP_401_UNAUTHORIZED
 
 
 # !Get all  trips
 @app.route("/trip/all", methods=["GET"])
 def all_trips():
-    trips = list(db["trips"]["trips"].find())
-    return json.dumps(trips, cls=DateTimeEncoder)
+    access_token = request.headers.get("auth")
+    if access_token:
+        permission = decode_token(access_token)
+        if permission != "admin":
+            return jsonify("Auth Failed: InValid or Expired JWT token"), status.HTTP_401_UNAUTHORIZED
+        else:
+            trips = list(db["trips"]["trips"].find())
+            return json.dumps(trips, cls=DateTimeEncoder)
+    else:
+        return jsonify("Auth Failed: No JWT Token Found"), status.HTTP_401_UNAUTHORIZED
 
-
-# !Trip Booking
+# !User Operations
+# !Trip Booking Search
 @app.route("/trip/search", methods=["POST"])
 def search_buses():
     start = request.json.get("start")
@@ -105,7 +165,7 @@ def search_buses():
     )
     return json.dumps(buses, cls=DateTimeEncoder)
 
-
+# !New Trip Booking
 @app.route("/trip/new", methods=["POST"])
 def new_trip():
     bus = str(request.json.get("bus"))
